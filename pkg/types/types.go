7ff95a0c088da2ea107397b7252751c163fae8e5
Leo <charmed_devil@list.ru>
2021-11-07 21:47:00 +0500

    добавлено поле status в структуру payment

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -3,6 +3,16 @@ package types
 //Money - денежная сумма в мин.еденицах (центы, копейки, дирамы и тд)
 type Money int64
 
+//Status - статус платежа
+type Status string
+
+//Предопределенные статусы платежей
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress = "INPROGRESS"
+)
+
 // Currency - категория в которой был совершен платеж(авто, аптеки, рестораны и тд)
 type Category string
 
@@ -11,4 +21,5 @@ type Payment struct {
 	ID			int
 	Amount 		Money
 	Category 	Category
+	Status		Status
 }
